9e83117242b76d54d26d59f1f9eaad068cc6baa3
akkhil2024 <akkhil2024@gmail.com>
2025-01-12 10:47:26 +0530

    Update TransactionService.java

--- services/Tolopolgyservice/src/main/java/com/tolopolgyservice/Tolopolgyservice/transactionproducer/service/TransactionService.java
+++ services/Tolopolgyservice/src/main/java/com/tolopolgyservice/Tolopolgyservice/transactionproducer/service/TransactionService.java
@@ -11,7 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.clients.producer.Callback;
 
 import java.util.Properties;
 
@@ -38,10 +40,26 @@ public class TransactionService {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule()); // Register module for LocalDateTime
         String transactionJson = mapper.writeValueAsString(transaction);
+        //ProducerRecord<TransactionKey, Transaction> producerRecord = new ProducerRecord<>(topic, 1,buildKey(transaction),transaction);
         ProducerRecord<TransactionKey, Transaction> producerRecord = new ProducerRecord<>(topic, buildKey(transaction),transaction);
 
         //
-        transactionKafkaProducer.send(producerRecord).get();
+      //  transactionKafkaProducer.send(producerRecord).get();
+        transactionKafkaProducer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                if (exception != null) {
+                    // Error occurred
+                    System.err.println("Error while producing message:=== " + exception.getMessage());
+                    // Implement fallback logic here, e.g., retry, log, or save to a file
+                } else {
+                    // Message successfully sent
+                    System.out.printf("Message sent to topic: %s, partition: %d, offset: %d%n",
+                            metadata.topic(), metadata.partition(), metadata.offset());
+                }
+            }
+        });
+
         System.out.println(" Messagd sent to Topic,,,,,,,, "+ topic+ "at "+ System.currentTimeMillis());
     }
 
