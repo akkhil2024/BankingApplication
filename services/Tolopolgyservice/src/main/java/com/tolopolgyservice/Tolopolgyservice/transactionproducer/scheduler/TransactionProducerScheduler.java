8d6034cc19b7e44db60a3f1d71b6b9f18f2e2d5a
akkhil2024 <akkhil2024@gmail.com>
2025-01-12 10:08:56 +0530

    Update TransactionProducerScheduler.java

--- services/Tolopolgyservice/src/main/java/com/tolopolgyservice/Tolopolgyservice/transactionproducer/scheduler/TransactionProducerScheduler.java
+++ services/Tolopolgyservice/src/main/java/com/tolopolgyservice/Tolopolgyservice/transactionproducer/scheduler/TransactionProducerScheduler.java
@@ -14,7 +14,7 @@ import java.util.Iterator;
 @RequiredArgsConstructor
 @Component
 public class TransactionProducerScheduler {
-    private static final String CSV_FILE = "/";
+    private static final String CSV_FILE = "/transactions.csv";
     private static final Iterator<Transaction> TRANSACTIONS = TransactionLoader.loadTransactions(CSV_FILE).iterator();
 
     private final TransactionService transactionService;
